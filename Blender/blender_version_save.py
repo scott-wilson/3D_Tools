546f27f49547f2f887daeb50edb417bdc673d9b7
Scott Wilson <gdragonmtn@gmail.com>
2013-01-21 23:59:02 -0800

    Organization and GPL
    Organized scripts into folders by software package and added the GPL header to all code.

---
+++ Blender/blender_version_save.py
@@ -0,0 +1,79 @@
+# blender_version_save.py (c) 2013 Scott Wilson (ProperSquid)
+#
+# ***** BEGIN GPL LICENSE BLOCK *****
+#
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.    See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# ***** END GPL LICENCE BLOCK *****
+
+bl_info = {
+    "name": "Version Save",
+    "author": "Scott Wilson",
+    "version": (1, 0, 0),
+    "blender": (2, 65, 0),
+    "location": "Info -> File Menu -> Save",
+    "description": "Automatically create versions of saved file every 5 minutes",
+    "warning": "",
+    "category": "System"}
+
+import bpy
+from time import time
+from os.path import split, join, exists, dirname, getctime
+
+class VersionSave(bpy.types.Operator):
+    bl_idname = 'file.version_save'
+    bl_label = 'Version Save'
+    
+    def execute(self, context):
+        # 1. Save scene, and set environment variables.
+        if bpy.data.filepath == '':
+            bpy.ops.wm.save_mainfile('INVOKE_AREA')
+        else:
+            bpy.ops.wm.save_as_mainfile('EXEC_AREA')
+            self.full_file_path = bpy.data.filepath
+            self.previous_time = getctime(self.full_file_path)
+            self.file_name = split(self.full_file_path)[1][:-6]
+            self.file_directory = dirname(self.full_file_path)
+        
+        # 2. Check if 5 minutes has passed since last save. If it has, save new version. Else, just save.
+        
+        
+        
+            if (time() - self.previous_time) >= 300: # Check if 5 minutes has passed (300 seconds).
+                try: # Check if there is already a number at the end of the scene.
+                    self.version_number = int(self.file_name[-4:])
+                    self.version_number += 1
+                    self.append_number = ''
+                    for i in range(4 - len(str(self.version_number))):
+                        self.append_number += '0'
+                    self.append_number += str(self.version_number)
+                    self.new_file_name = self.file_name[:-4] + self.append_number
+                except ValueError: # If there isn't, add a number.
+                    self.new_file_name = self.file_name + '-0001'
+                bpy.ops.wm.save_as_mainfile(filepath = join(self.file_directory, self.new_file_name + '.blend'))
+
+        return {'FINISHED'}
+
+# Registration
+
+def register():
+    bpy.utils.register_class(VersionSave)
+
+def unregister():
+    bpy.utils.unregister_class(VersionSave)
+
+if __name__ == "__main__":
+    register() 
---
+++ Softimage/softimage_set_object_new_zero.py
@@ -0,0 +1,44 @@
+# softimage_set_object_new_zero.py (c) 2013 Scott Wilson (ProperSquid)
+#
+# ***** BEGIN GPL LICENSE BLOCK *****
+#
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.    See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# ***** END GPL LICENCE BLOCK *****
+
+"""
+Created by Scott Wilson
+Version 1.0
+Date 1/11/2013
+"""
+
+objects_list = []
+
+for object in Application.Selection:
+  objects_list.append(object)
+
+if objects_list > 0:
+	for object in objects_list:
+		Application.GetPrim("Null", "", "", "")
+		new_null = Application.GetValue(Application.Selection(0))
+		Application.MatchTransform(new_null, object, "siSRT", "")
+		if Application.ActiveSceneRoot.Name != object.parent.Name:
+			Application.ParentObj(object.parent, new_null)
+		Application.ParentObj(new_null, object)
+		Application.SetValue("{null_object}.visibility.viewvis".format(null_object = new_null), False, "")
+		Application.SetValue("{null_object}.visibility.rendvis".format(null_object = new_null), False, "")
+		Application.SetValue("{null_object}.Name".format(null_object = new_null), "{object_name}_data".format(object_name = object), "")
+		
