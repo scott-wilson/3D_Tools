7e91577f19118ce3ca1206a5612abc1cbfc2ce4f
propersquid <gdragonmtn@gmail.com>
2013-03-06 15:25:03 -0800

    Update blender_version_save.py
    Didn't update.

--- Blender/blender_version_save.py
+++ Blender/blender_version_save.py
@@ -22,8 +22,8 @@
 bl_info = {
     "name": "Version Save",
     "author": "Scott Wilson",
-    "version": (1, 0, 0),
-    "blender": (2, 65, 0),
+    "version": (1, 1, 0),
+    "blender": (2, 66, 1),
     "location": "Info -> File Menu -> Save",
     "description": "Automatically create versions of saved file every 5 minutes",
     "warning": "",
@@ -49,18 +49,9 @@ class VersionSave(bpy.types.Operator):
             self.previous_time = getctime(self.full_file_path)
             self.file_name = split(self.full_file_path)[1][:-6]
             self.file_directory = dirname(self.full_file_path)
+
             # 2. Check if 5 minutes has passed since last save. If it has, save new version. Else, just save.
-            if (time() - self.previous_time) >= 3: # Check if 5 minutes has passed (300 seconds).
-#                try: # Check if there is already a number at the end of the scene.
-#                    self.version_number = int(self.file_name[-4:])
-#                    self.version_number += 1
-#                    self.append_number = ''
-#                    for i in range(4 - len(str(self.version_number))):
-#                        self.append_number += '0'
-#                    self.append_number += str(self.version_number)
-#                    self.new_file_name = self.file_name[:-4] + self.append_number
-#                except ValueError: # If there isn't, add a number.
-#                    self.new_file_name = self.file_name + '-0001'
+            if (time() - self.previous_time) >= 300: # Check if 5 minutes has passed (300 seconds).
                 self.version_number = '-0001'
                 while exists(join(self.file_directory, self.file_name + self.version_number + '.blend')) == True:
                     self.version_number = int(self.version_number[1:])
@@ -71,7 +62,6 @@ class VersionSave(bpy.types.Operator):
                     self.append_number += str(self.version_number)
                     self.version_number = '-' + self.append_number
                 self.new_file_name = self.file_name + self.version_number
-#                bpy.ops.wm.save_as_mainfile(filepath = join(self.file_directory, self.new_file_name + '.blend'))
                 copyfile(join(self.file_directory, self.file_name + '.blend'), join(self.file_directory, self.new_file_name + '.blend'))
 
         return {'FINISHED'}
