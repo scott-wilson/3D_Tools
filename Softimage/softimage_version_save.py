a0d57ccc20c83e9ce864d43349f60d1ec2233f2c
propersquid <gdragonmtn@gmail.com>
2013-04-24 11:41:28 -0700

    Update softimage_version_save.py

--- Softimage/softimage_version_save.py
+++ Softimage/softimage_version_save.py
@@ -21,12 +21,13 @@
 
 """
 Created by Scott Wilson
-Version 2.0
-Date 1/18/2013
+Version 2.1
+Date 4/22/2013
 """
 
 from time import time
 from os.path import split, join, exists, dirname, getctime
+from shutil import copyfile
 
 # 1. Save scene, and set environment variables.
 Application.SaveScene()
@@ -35,24 +36,26 @@ full_file_path = Application.ActiveProject3.ActiveScene.filename.value
 file_name = split(full_file_path)[1][:-4]
 file_directory = dirname(full_file_path)
 
-# 2. Check if 5 minutes has passed since last save. If it has, save new version. Else, just save.
+    # 2. Check if 5 minutes has passed since last save. If it has, duplicate.
 
-previous_time = getctime(full_file_path)
-
-
-if (time() - previous_time) >= 300: # Check if 5 minutes has passed (300 seconds).
-	try: # Check if there is already a number at the end of the scene.
-		version_number = int(file_name[-4:])
-		version_number += 1
-		append_number = ''
-		for i in range(4 - len(str(version_number))):
-			append_number += '0'
-		append_number += str(version_number)
-		new_file_name = file_name[:-4] + append_number
-	except ValueError: # If there isn't, add a number.
-		new_file_name = file_name + '-0001'
-	Application.SaveSceneAs(join(file_directory, new_file_name + '.scn'))
-	
-#	with open(version_file, 'w') as f:
-#		f.write(str(time()))
-
+version_number = '-0001'
+old_version_number = version_number
+while exists(join(file_directory, file_name + version_number + '.scn')):
+    old_version_number = version_number
+    version_number = int(version_number[1:])
+    version_number += 1
+    append_number = ''
+    for i in range(4 - len(str(version_number))):
+        append_number += '0'
+    append_number += str(version_number)
+    version_number = '-' + append_number
+    
+try:
+    previous_time = getctime(join(file_directory, file_name + old_version_number + '.scn'))
+    
+except WindowsError:
+    previous_time = getctime(full_file_path)
+    
+if(time() - previous_time) >= 300:
+    new_file_name = file_name + version_number
+    copyfile(join(file_directory, file_name + '.scn'), join(file_directory, new_file_name + '.scn'))
