9a65b4bb070e582f4ed5603c39e63428cbe60835
propersquid <gdragonmtn@gmail.com>
2013-04-24 11:38:05 -0700

    Update maya_version_save.py

--- Maya/maya_version_save.py
+++ Maya/maya_version_save.py
@@ -21,50 +21,54 @@
 
 """
 Created by Scott Wilson
-Version 1.0
-Date 1/23/2013
+Version 1.1
+Date 3/20/2013
 """
 
 from time import time
 from os.path import split, join, exists, dirname, getctime
+from shutil import copyfile
 import maya.cmds as cmds
 
 # 1. Save scene, and set environment variables.
-if cmds.file(query = True, sceneName = True) == '':
+if cmds.file(query = True, sceneName = True) == None:
     file_filters = "Maya ASCII (*.ma);;Maya Binary (*.mb)"
-    get_save_settings = cmds.fileDialog2(fileFilter=multipleFilters, dialogStyle=2, returnFilter = True, selectFileFilter = 'Maya Binary (*.mb)')
+    get_save_settings = cmds.fileDialog2(fileFilter=file_filters, dialogStyle=2, returnFilter = True, selectFileFilter = 'Maya Binary (*.mb)')
     cmds.file(rename = get_save_settings[0])
     if get_save_settings[1] == 'Maya ASCII':
         cmds.file(save = True, type = 'mayaAscii')
+        file_ext = '.ma'
     elif get_save_settings[1] == 'Maya Binary':
         cmds.file(save = True, type = 'mayaBinary')
+        file_ext = '.mb'
 else:
     cmds.file(save = True)
-full_file_path = cmds.file(query = True, sceneName = True)
-file_type = cmds.file(query = True, type = True)[0]
-file_name = split(full_file_path)[1][:-3]
-file_directory = dirname(full_file_path)
-
-# 2. Check if 5 minutes has passed since last save. If it has, save new version. Else, just save.
-
-previous_time = getctime(full_file_path)
-
-
-if (time() - previous_time) >= 300: # Check if 5 minutes has passed (300 seconds).
-	try: # Check if there is already a number at the end of the scene.
-		version_number = int(file_name[-4:])
-		version_number += 1
-		append_number = ''
-		for i in range(4 - len(str(version_number))):
-			append_number += '0'
-		append_number += str(version_number)
-		new_file_name = file_name[:-4] + append_number
-	except ValueError: # If there isn't, add a number.
-		new_file_name = file_name + '-0001'
-	#Application.SaveSceneAs(join(file_directory, new_file_name + '.scn'))
-	if file_type == 'mayaAscii':
-            cmds.file(rename = join(file_directory, new_file_name + '.ma'))
-            cmds.file(save = True)
-        elif file_type == 'mayaBinary':
-            cmds.file(rename = join(file_directory, new_file_name + '.mb'))
-            cmds.file(save = True)
+    full_file_path = cmds.file(query = True, sceneName = True)
+    file_type = cmds.file(query = True, type = True)[0]
+    file_name = split(full_file_path)[1][:-3]
+    file_directory = dirname(full_file_path)
+    file_ext = split(full_file_path)[1][-3:]
+    
+    # 2. Check if 5 minutes has passed since last save. If it has, duplicate.
+    
+    version_number = '-0001'
+    old_version_number = version_number
+    while exists(join(file_directory, file_name + version_number + file_ext)):
+        old_version_number = version_number
+        version_number = int(version_number[1:])
+        version_number += 1
+        append_number = ''
+        for i in range(4 - len(str(version_number))):
+            append_number += '0'
+        append_number += str(version_number)
+        version_number = '-' + append_number
+        
+    try:
+        previous_time = getctime(join(file_directory, file_name + old_version_number + file_ext))
+        
+    except WindowsError:
+        previous_time = getctime(full_file_path)
+        
+    if(time() - previous_time) >= 300:
+        new_file_name = file_name + version_number
+        copyfile(join(file_directory, file_name + file_ext), join(file_directory, new_file_name + file_ext))
